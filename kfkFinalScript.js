4ae3542f9dcfedda319d701f241a70a94b82558e
ShaunK79 <wism79@gmail.com>
2025-04-23 20:02:18 +0100

    changes

--- kfkFinalScript.js
+++ kfkFinalScript.js
@@ -7,34 +7,34 @@ function toggleMenu() {
 }
 
 // navbar fixed transparent to solid
-    window.addEventListener('scroll', () => {
-        const navbar = document.querySelector('.navbar');
-        if (window.scrollY > 50) { 
-            navbar.classList.add('solid');
-            navbar.classList.remove('transparent');
-        } else {
-            navbar.classList.add('transparent');
-            navbar.classList.remove('solid');
-        }
-    });
-
-
-
-        // Scroll up button
-    // Show button after scrolling down 100px
-  window.onscroll = () => {
-    const button = document.getElementById("backToTop");
-    if (window.scrollY > 300) {
-      button.classList.add('show');
-    } else {
-      button.classList.remove('show');
-    }
-  };
-
-  // Smooth scroll back to top
-  function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('solid');
+    navbar.classList.remove('transparent');
+  } else {
+    navbar.classList.add('transparent');
+    navbar.classList.remove('solid');
   }
+});
+
+
+
+// Scroll up button
+// Show button after scrolling down 100px
+window.addEventListener('scroll', function () {
+  const backToTop = document.getElementById('backToTop');
+  if (window.pageYOffset > 300) {
+    backToTop.classList.add('show');
+  } else {
+    backToTop.classList.remove('show');
+  }
+});
+
+// Smooth scroll back to top
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
